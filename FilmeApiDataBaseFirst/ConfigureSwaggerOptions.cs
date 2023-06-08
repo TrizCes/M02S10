72b4719a016a8b6707de6aa24a8a0ddd25afc811
Beatriz Ceschini <biaceschini@gmail.com>
2023-06-07 21:20:28 -0300

    Criação do projeto FilmesAPI com uso do database first

---
+++ FilmeApiDataBaseFirst/ConfigureSwaggerOptions.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmeApiDataBaseFirst
+{
+    public class ConfigureSwaggerOptions : IConfigureNamedOptions<SwaggerGenOptions>
+    {
+        private readonly IApiVersionDescriptionProvider provider;
+
+        public ConfigureSwaggerOptions(
+                IApiVersionDescriptionProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        public void Configure(SwaggerGenOptions options)
+        {
+                foreach (var description in provider.ApiVersionDescriptions)
+                {
+                    options.SwaggerDoc(
+                        description.GroupName,
+                        CreateVersionInfo(description));
+                }
+        }
+
+            public void Configure(string name, SwaggerGenOptions options)
+            {
+                Configure(options);
+            }
+
+            private OpenApiInfo CreateVersionInfo(
+                    ApiVersionDescription description)
+            {
+                var info = new OpenApiInfo()
+                {
+                    Title = "University",
+                    Version = description.ApiVersion.ToString()
+                };
+
+                if (description.IsDeprecated)
+                {
+                    info.Description += " Essa versão está depreciada.";
+                }
+
+                return info;
+            }
+    } 
+}
